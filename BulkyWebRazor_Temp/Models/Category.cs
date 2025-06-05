06252805d4d9521c43e93f336dce5a6a4b669375
Anush <taherianush@gmail.com>
2025-06-05 18:48:33 +0330

    add UI And Classibraries

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(20, ErrorMessage = "The Max Length Is 20 Char!")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The Display Orde Must Between 1 To 100!")]
+        public int DisplayOrder { get; set; }
+    }
+}
